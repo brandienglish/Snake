50914ac4a1e50b7104b4a179b512e8c48f0c0b7b
brandienglish <brandienglish87@gmail.com>
2024-03-12 16:37:21 -0500

    Basics of game Completed.

--- snake.py
+++ snake.py
@@ -13,26 +13,34 @@ number_of_cells=25
 
 # Food class 
 class Food:
-    def __init__(self) :
-        self.position= self.generate_random_pos()
+    def __init__(self, snake_body) :
+        self.position= self.generate_random_pos(snake_body)
 
     #displaying and positiong the food 
     def draw(self):
-        food_rect= pygame.Rect(self.position.x *cell_size,self.position.y *cell_size,cell_size,cell_size)
+        food_rect= pygame.Rect(self.position.x * cell_size, self.position.y *cell_size,cell_size,cell_size)
         screen.blit(food_image, food_rect)
     
-    # generating the food in random spots
-    def generate_random_pos(self):
-        x=random.randint(0, number_of_cells -1)
+    def generate_random_cell(self):
+        x=random.randint(0, number_of_cells-1)
         y= random.randint(0, number_of_cells-1)
-        position = Vector2(x,y)
+        return     Vector2(x,y)
+    
+    # generating the food in random positions
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+        while position in snake_body:
+            position = self.generate_random_cell()
         return position
+
+       
 
 # class for snake 
 class Snake:
     def __init__(self):
         self.body=[Vector2(6,9),  Vector2(5,9), Vector2(4,9)]
         self.direction= Vector2(1,0)
+        self.add_segment = False
 
     def draw(self):
         for segment in self.body:
@@ -40,21 +48,59 @@ class Snake:
             pygame.draw.rect(screen,DARK_GREEN, segment_rect,0,10)
     
     def update(self):
-        self.body= self.body[:-1]
+         #adding a segment to the head of the snake 
         self.body.insert(0,self.body[0]+self.direction)
+        if self.add_segment == True:  
+            self.add_segment = False
+        else:
+            self.body= self.body[:-1]
+
+    def reset(self):
+        # goes back to orginal body length 
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]    
+        self.direction = Vector2(1,0)
 
 class Game:
     def __init__(self):
         self.snake= Snake()
-        self.food = Food()
+        self.food = Food(self.snake.body)
+        self.state = "RUNNING"
 
     def draw(self):
         self.food.draw()
         self.snake.draw()
     
     def update(self):
-        self.snake.update()
+        if self.state =="RUNNING":
+            self.snake.update()
+            self.collision_with_food()
+            self.collision_with_edges()
+            self.collision_with_tail()
+
+    def collision_with_food(self):
+        if self.snake.body[0] == self.food.position:
+            #food doesnt spawn on snake 
+            self.food.position = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
     
+    def collision_with_edges(self):
+        if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
+            self.game_over()
+        if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1:
+            self.game_over()
+
+    def game_over(self):
+        self.snake.reset()
+        #moving food to new position 
+        self.food.position = self.food.generate_random_pos(self.snake.body)
+        self.state = "STOPPED"
+
+    def collision_with_tail(self):
+        no_head = self.snake.body[1:]
+        if self.snake.body[0] in no_head:
+            self.game_over()
+
+
 #creating the game display screen (game window)
 screen = pygame.display.set_mode((cell_size*number_of_cells,cell_size*number_of_cells))
 
@@ -79,6 +125,11 @@ while True:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
+            # triggers new game once player press any key 
+            #the game resets
+            if game.state == "STOPPED":
+                game.state = "RUNNING"
+            ###
             if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
                 #decrease the y cord to move up
                 game.snake.direction = Vector2(0,-1)
@@ -98,7 +149,7 @@ while True:
      
     #drawing 
     screen.fill(GREEN)
-    game.draw()
+    game.draw() 
     pygame.display.update()
 
     #game runs 60 frames per second
